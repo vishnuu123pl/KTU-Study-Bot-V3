42b2b3e3fde4f9e047ebbf595fbe2e1582bd92eb
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-20 20:37:16 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,24 +1,34 @@
 import os
-import asyncio
 import threading
 from flask import Flask
-from bot import main
+from bot import app, main
 
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
 def home():
-    return "Bot Running"
+    return "Bot is Running"
+
 
 def run_flask():
     port = int(os.environ.get("PORT", 8000))
-    flask_app.run(host="0.0.0.0", port=port, use_reloader=False)
+
+    flask_app.run(
+        host="0.0.0.0",
+        port=port,
+        use_reloader=False
+    )
+
 
 if __name__ == "__main__":
-    # Start Flask in a background thread
-    flask_thread = threading.Thread(target=run_flask, daemon=True)
-    flask_thread.start()
-    print("✅ Flask thread started")
 
-    # Run the bot in the main thread (required for Pyrogram signal handling)
-    asyncio.run(main())
+    flask_thread = threading.Thread(
+        target=run_flask,
+        daemon=True
+    )
+
+    flask_thread.start()
+
+    print("Flask Started")
+
+    app.run(main())
