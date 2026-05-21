1fc090430eac9a3f070cdb81ea5c502c083a1ef0
VISHNUUUUUUUU <137207623+vishnuu123pl@users.noreply.github.com>
2026-05-21 15:31:10 +0530

    Update back.py

--- plugins/back.py
+++ plugins/back.py
@@ -1,8 +1,20 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.start import START_TEXT, START_BUTTONS
 
-@Client.on_callback_query(filters.regex("^back_start$"))
-async def back_start(_, query):
-    await query.message.edit_text(START_TEXT, reply_markup=START_BUTTONS)
+
+@Client.on_callback_query(
+    filters.regex("^back_home$")
+)
+async def back_home(_, query):
+
+    try:
+
+        await query.message.edit_text(
+            START_TEXT,
+            reply_markup=START_BUTTONS
+        )
+
+    except:
+        pass
+
     await query.answer()
